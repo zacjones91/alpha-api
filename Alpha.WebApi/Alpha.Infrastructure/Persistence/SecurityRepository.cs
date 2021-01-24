a15d0c54b1220cf10e8ef238d6e5b41cf44207a1
Zac Jones <zacjones91@gmail.com>
2021-01-24 11:22:54 -0600

    Add startup

---
+++ Alpha.WebApi/Alpha.Infrastructure/Persistence/SecurityRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Alpha.Common.Interfaces;
+
+namespace Alpha.Infrastructure.Persistence
+{
+    public class SecurityRepository : ISecurityRepository
+    {
+        public Task<Tuple<bool, int>> CheckSecurityExists(string symbol)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Alpha.WebApi/Alpha.WebApi/Startup.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Alpha.Common.Interfaces;
+using Alpha.Infrastructure.Persistence;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: FunctionsStartup(typeof(Alpha.WebApi.Startup))]
+
+namespace Alpha.WebApi
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            builder
+                .Services
+                .AddTransient<ITransactionRepository, TransactionRepository>();
+        }
+    }
+}
