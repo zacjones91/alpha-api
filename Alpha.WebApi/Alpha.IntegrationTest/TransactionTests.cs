3d4eb3e88ef524655c22511a2db42671d9575636
Zac Jones <zacjones91@gmail.com>
2021-01-24 11:45:31 -0600

    Add test project

---
+++ Alpha.WebApi/Alpha.IntegrationTest/TransactionTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Alpha.IntegrationTest
+{
+    public class TransactionTests
+    {
+        [Fact]
+        public void AddTransactionAsync()
+        {
+            // arrange
+
+            // act
+
+            // assert
+            Assert.True(true);
+        }
+    }
+}
